14c331ece9b232f09a079dbe8342d609eb15a7e6
MARIA <maria@MARIAs-MacBook-Pro.local>
2021-01-15 23:45:08 +0100

    starting FrontEnd

--- frontnd/src/App.js
+++ frontnd/src/App.js
@@ -1,10 +1,36 @@
 
-function App() {
+import React from 'react'
+import {BrowserRouter as Router , Route} from 'react-router-dom'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import { Container} from 'react-bootstrap'
+
+import HomeScreen from './Screens/HomeScreen'
+
+import ProductScreen from './Screens/ProductScreen'
+
+
+
+const App = () => {
   return (
-    <>
-      heloo
-    </>
-  );
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Route  path='/'  component={HomeScreen} exact/>
+          
+          <Route  path='/product/:id'  component={ProductScreen}/>
+
+
+        </Container>
+        
+      </main>
+      <Footer/>
+
+    </Router>
+  )
 }
 
-export default App;
+export default App
